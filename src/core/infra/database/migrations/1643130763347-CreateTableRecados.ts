1dfc3c9372fb72ea9183122d8f4f80d70ec6b92c
STUMPF1988 <stumpfdaniele@gmail.com>
2022-01-25 14:15:55 -0300

    alteracao_tabela_recados

--- src/core/infra/database/migrations/1643130763347-CreateTableRecados.ts
+++ src/core/infra/database/migrations/1643130763347-CreateTableRecados.ts
@@ -1,10 +1,9 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class CreateTableRecados1642810645103 implements MigrationInterface {
-    name = 'CreateTableRecados1642810645103'
+export class CreateTableRecados1643130763347 implements MigrationInterface {
+    name = 'CreateTableRecados1643130763347'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        
         await queryRunner.query(`CREATE TABLE "recados" ("uid" uuid NOT NULL DEFAULT uuid_generate_v4(), "descricao" character varying NOT NULL, "detalhamento" character varying, "created_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_4c05a5828e0b6fee40a7e78e6e8" PRIMARY KEY ("uid"))`);
     }
 
